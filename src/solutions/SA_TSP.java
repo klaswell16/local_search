f2004341aa6d1e9e3b1bfb837e952e5ffb89bd0b
Kayan Laswell <kcclaswell@gmail.com>
2025-02-25 11:00:54 -0500

    Added unoptimal SA to SA_TSP

--- src/solutions/SA_TSP.java
+++ src/solutions/SA_TSP.java
@@ -1,15 +1,23 @@
 package solutions;
 
+import core_algorithms.SimulatedAnnealing;
+
 import problems.TSP;
 
-public class SA_TSP {
-    public static void main(String[] args) {
-        int sample = 5;
-        TSP tsp = new TSP(sample);
-        int[] initialState = tsp.getInitState();
-        tsp.printState(initialState);
+public class SA_TSP extends SimulatedAnnealing<int[] > {
+    private final static long INIT_TIME = 1;
+    private final static double INIT_TEMP = 1e13;
+    private final static long MAX_TIME = 100_000_000;
+    public SA_TSP(TSP problem){
+        super(INIT_TIME, INIT_TEMP,  problem);
+    }
 
-        int[] newState = tsp.generateNewState(initialState);
-        tsp.printState(newState);
+    public double schedule(long time, double temp){
+        return temp*(1-time/(double)MAX_TIME);
+    }
+
+    public static void main(String[] args) {
+        SA_TSP agent= new SA_TSP(new TSP(17));
+        agent.search();
     }
 }
